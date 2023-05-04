b86bf784b57b343a93b9bf25e3ee462246599d12
Gaurav Soni <120136122+gaurav8158@users.noreply.github.com>
2023-05-05 01:17:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,17 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+	 const prices = document.querySelectorAll('.price');
+  let totalPrice = 0;
+  prices.forEach(price => {
+    totalPrice += parseFloat(price.textContent);
+  });
+  const totalRow = document.createElement('tr');
+  const totalCell = document.createElement('td');
+  totalCell.colSpan = 2;
+  totalCell.textContent = `Total: ${totalPrice.toFixed(2)}`;
+  totalRow.appendChild(totalCell);
+  document.querySelector('tbody').appendChild(totalRow);
 };
 
 getSumBtn.addEventListener("click", getSum);
